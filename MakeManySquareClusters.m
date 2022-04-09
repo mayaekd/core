23d2fe009f542cef25adb299b5d3bf583eb2bccd
Maya Davis <59815270+mayaekd@users.noreply.github.com>
2022-04-09 23:45:31 +0300

    Make Many Square Clusters Update

--- MakeManySquareClusters.m
+++ MakeManySquareClusters.m
@@ -4,28 +4,30 @@
 % Concept by Maya Davis and Melissa A. Redford
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function ClusterList = MakeManySquareClusters(xStart, yStart, ...
-    StepSize, SideLength, DistanceBetweenClusters, NumClustersPerSide, ...
+function ClusterList = MakeManySquareClusters(xStart_h, StepSize_h, ...
+    SideLength_h, DistanceBetweenClusters_h, NumClustersPerSide_h, ...
+    yStart_v, StepSize_v, SideLength_v, DistanceBetweenClusters_v, ...
+    NumClustersPerSide_v, ...
     SpaceMPTransformation)
-    ClusterList = cell(NumClustersPerSide^2,1);
+    ClusterList = cell(NumClustersPerSide_h*NumClustersPerSide_v,1);
     ClusterIndex = 1;
-    yMin = yStart;
-    yMax = yStart + SideLength;
-    yStep = StepSize;
-    for w = 1:NumClustersPerSide
-        xMin = xStart;
-        xMax = xStart + SideLength;
-        xStep = StepSize;
-        for h = 1:NumClustersPerSide
+    yMin = yStart_v;
+    yMax = yStart_v + SideLength_v;
+    yStep = StepSize_v;
+    for w = 1:NumClustersPerSide_v
+        xMin = xStart_h;
+        xMax = xStart_h + SideLength_h;
+        xStep = StepSize_h;
+        for h = 1:NumClustersPerSide_h
             CurrentCluster = MakeSquareCluster(xMin, xMax, yMin, yMax, ...
                 xStep, yStep, SpaceMPTransformation);
             ClusterList{ClusterIndex, 1} = CurrentCluster;
-            xMin = xMax + DistanceBetweenClusters;
-            xMax = xMin + SideLength;
+            xMin = xMax + DistanceBetweenClusters_h;
+            xMax = xMin + SideLength_h;
             ClusterIndex = ClusterIndex + 1;
         end
-        yMin = yMax + DistanceBetweenClusters;
-        yMax = yMin + SideLength;
+        yMin = yMax + DistanceBetweenClusters_v;
+        yMax = yMin + SideLength_v;
     end
 end
 
